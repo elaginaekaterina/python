6196763173d4553dd1c844754eb8ccc8ae95581f
elaginaekaterina <74530377+elaginaekaterina@users.noreply.github.com>
2020-12-04 13:41:59 +0300

    Add files via upload

---
+++ Операторы. Переменные. Тип данных. Условия/Строки(практика).py
@@ -0,0 +1,16 @@
+'''
+a = 'string'
+b = 'another string'
+print(a, b)
+print(a + b)
+print(a)
+print(b)
+print(a + '\n' + b) # print in two different lines
+'''
+
+'''
+a = "123"
+b = "42"
+print(a + b)
+'''
+
---
+++ Операторы. Переменные. Тип данных. Условия/Строки.py
@@ -0,0 +1,31 @@
+# Строки
+'string1'
+"string"
+#на нескольких строках
+''' multiple lines
+string'''
+""" multiple lines
+string with double qoutes"""
+# Операции со строками
+сложение:
+    'abc' + 'def' = 'abcdef'
+умножение:
+    'abc' * 3 = 'abcabcabc'
+длина строки кол-во символов:
+    len('abcdef') = 6
+сравнение строк: сравниваются по алфавиту:
+    'abc' == '''abc'''
+    'abc' < 'ac'
+    'abc' > 'ab'
+'\n' - символ перевода строки
+print('First line', '\n\n\n', 'Last line')
+Результат:
+    First line
+
+
+    Last line
+    
+'''
+Многострочный коментарий как строки 
+'''
+
